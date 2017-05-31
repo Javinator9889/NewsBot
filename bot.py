91a8e4f0b54366615596e01c30d890acefaea4d8
Javier <javialonso007@hotmail.es>
2017-05-31 19:43:46 +0200

    Added license

--- bot.py
+++ bot.py
@@ -75,6 +75,12 @@ updater.start_polling(timeout=30)
 print("Starting bot...\n\n")
 
 print("Iniciando programación...")
+
+print("NewsBot (Telegram)  Copyright (C) 2017  Javinator9889\n\n\
+This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.\n\
+This is free software, and you are welcome to redistribute it\
+under certain conditions; type `show c' for details.")
+
 try:
     while 1:
         data = db_m.fetch_chatids_progtimes()
@@ -130,3 +136,24 @@ try:
 except (KeyboardInterrupt, TypeError):
     updater.idle()        # Stops the bot only with "Ctrl+C" on keyboard
     print("\nBot detenido\nFinalizando...")
+
+# The next message has to be included in every copy of this program, modified or not
+""" NewsBot (Telegram) -- A simple bot for getting news from Google
+    Copyright (C) 2017  Javinator9889
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    For contacting, go to "https://github.com/Javinator9889/NewsBot/issues" and type your message.
+    Also you can go to my GitHub profile and send me direct message.
+"""
