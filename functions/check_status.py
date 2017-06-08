d2796f50e38462510b1c0df9bf916ceee463ff96
Javier <javialonso007@hotmail.es>
2017-06-08 21:36:04 +0200

    Delete check_status.py

--- functions/check_status.py
+++
@@ -1,7 +0,0 @@
-import urllib.request as rq
-import requests
-
-request = requests.get('http://altadensidad.com/%3Fp%3D98195')
-print(request.status_code)
-
-#print(rq.urlopen("http://altadensidad.com/%3Fp%3D98195").getcode())
